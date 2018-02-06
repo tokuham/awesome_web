74125b15a0569751524a871a73f7bde2bbfcfc02
Tokuham <tokuham@gmail.com>
2018-02-06 15:01:23 +0800

    update a mistake

--- www/models.py
+++ www/models.py
@@ -4,7 +4,9 @@
 
 import time, uuid
 
-from orm import Model, StringField, BooleanField, FloatField, TextField
+#import www.orm
+
+from www.orm import Model, StringField, BooleanField, FloatField, TextField
 
 def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
--- www/orm.py
+++ www/orm.py
@@ -61,6 +61,7 @@ def create_args_string(num):
         L.append('?')
     return ', '.join(L)
 
+
 class Field(object):
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
@@ -71,25 +72,31 @@ class Field(object):
     def __str__(self):
         return '<%s,%s,%s>' % self.__class__.__name__, self.column_type, self.name
 
+
 class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
+
 
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
+
 class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
+
 
 class FloatField(Field):
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
+
 class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
+
 
 class ModelMetaclass(type):
 
